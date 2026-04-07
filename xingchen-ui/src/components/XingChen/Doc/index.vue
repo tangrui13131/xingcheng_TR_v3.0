990f3a62e07ba98d8d3a0f7cc51b0f3d93f3ec38
ruihui-c-language <372491879@qq.com>
2026-04-07 18:07:36 +0800

    1

---
+++ xingchen-ui/src/components/XingChen/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XingChenDoc',
+  data() {
+    return {
+      url: 'http://doc.xingchen.vip/xingchen-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ xingchen-ui/src/components/XingChen/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XingChenGit',
+  data() {
+    return {
+      url: 'https://gitee.com/y_project/XingChen-Vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
